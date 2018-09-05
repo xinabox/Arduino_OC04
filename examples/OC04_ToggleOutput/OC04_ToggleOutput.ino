640874762149546670060e3299ee89eabbfb3ee1
brendanvanbreda <brendanvanbreda1993@gmail.com>
2018-09-05 18:00:29 +0200

    add source files

---
+++ examples/OC04_ToggleOutput/OC04_ToggleOutput.ino
@@ -0,0 +1,51 @@
+/*************************************************************
+	This is an examples for the OC04
+	Relay Out High Voltage
+	
+	You can buy one on our store!
+	-----> https://xinabox.cc/products/OC04/
+	
+	This example in structs the OC04 to turn its output port
+	on and off
+	
+	The sensor communicates over the I2C Bus.
+	
+*************************************************************/
+
+/********************* Library Includes *********************/
+#include <xOC04.h> // https://github.com/xinabox/xOC04
+#include <xCore.h> // https://github.com/xinabox/xCore
+
+/****************** Global sensor objects *******************/
+xOC04 OC04;
+
+/********************* SYSTEM VARIABLES *********************/
+const int DELAY_TIME = 2000;
+
+/*********************** Sketch Code ************************/
+void setup() {
+	// Start the Serial Monitor
+	Serial.begin(115200);
+
+	#ifdef ESP8266
+		Wire.pins(2,14);
+	#endif
+		
+	// Start the I2C Communication
+	Wire.begin();
+	
+	// Start the OC04 port expander
+	OC04.begin();
+
+	Serial.println("OC04 Test");
+}
+
+void loop() {
+	// Switch OUT on
+	OC04.write(HIGH);
+	delay(DELAY_TIME);
+	
+	// Switch OUT off
+	OC04.write(LOW);
+	delay(DELAY_TIME);
+}
---
+++ src/xOC04.cpp
@@ -0,0 +1,65 @@
+/*
+	This is a library for the OC04
+	Relay Out High Voltage
+
+	The board uses I2C for communication.
+	The board communicates with the following I2C device:
+	* 	PCA9554A
+	
+	Data Sheets:
+	PCA9554A - http://www.ti.com/lit/ds/symlink/pca9554a.pdf
+*/
+
+#include <xOC04.h>
+
+/********************************************************
+ 	Constructor
+*********************************************************/
+xOC04::xOC04()
+{
+	PCA9554A_I2C_ADDRESS = 0x38;
+}
+
+xOC04::xOC04(uint8_t addr)
+{
+	PCA9554A_I2C_ADDRESS = addr;
+}
+
+/********************************************************
+ 	Configure OC04
+*********************************************************/
+bool xOC04::begin(void)
+{
+	begin(PCA9554A_ALL_OUTPUTS_OFF);
+}
+
+bool xOC04::begin(bool state)
+{
+	write(state);
+	xCore.write8(PCA9554A_I2C_ADDRESS, PCA9554A_REG_CONFIG, PCA9554A_CONF_OUTPUT);
+	return true;
+}
+
+/********************************************************
+ 	Write to PIN
+*********************************************************/
+void xOC04::write(bool state)
+{	
+	if(state == true){
+		xCore.write8(PCA9554A_I2C_ADDRESS, PCA9554A_REG_OUTPUT_PORT, 0x01);
+	}else if(state == false){
+		xCore.write8(PCA9554A_I2C_ADDRESS, PCA9554A_REG_OUTPUT_PORT, 0x00);
+	}
+}
+
+/********************************************************
+ 	Get output pin Status
+*********************************************************/
+uint8_t xOC04::getStatus(void)
+{
+	uint8_t pin_state = xCore.read8(PCA9554A_I2C_ADDRESS, PCA9554A_REG_OUTPUT_PORT); 
+	return pin_state;
+}
+
+
+
